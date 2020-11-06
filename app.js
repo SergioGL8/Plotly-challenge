ffdb816e193f583c306e1a5aec22438bb2b085c7
SergioGL <67619423+SergioGL8@users.noreply.github.com>
2020-11-06 00:12:08 -0600

    Plotly

--- app.js
+++ app.js
@@ -1,102 +1,107 @@
-// append ids to the dropdown   
-d3.json('data/samples.json').then((data)=>{
-    var id=data.names;
+// Get the names to the dropdown 
+d3.json("data/samples.json").then((data) => {
+    var names = data.names;
     console.log(data.metadata);
-    var select=d3.selectAll('#selDataset');
-    Object.entries(id).forEach(([i,v])=>{
-        select.append('option').text(v);
+
+    var select = d3.selectAll("#selDataset");
+    Object.entries(names).forEach(([i,v]) => {
+        select.append("option").text(v);
     })
 })
-function makePlot(testId){
-    d3.json('data.json').then((data)=>{
-        // This is the array
-        var samples=data.samples;
-        var testNum=samples.map(row=>row.id).indexOf(testId);
-        // Make bar plot
-        var otuValueTen=samples.map(row=>row.sample_values);
-        var otuValueTen=otuValueTen[testNum].slice(0,10).reverse();
-        var otuIdTen=samples.map(row=>row.otu_ids);
-        var otuIdTen=otuIdTen[testNum].slice(0,10);
-        var otuLabelTen=samples.map(row=>row.otu_labels); 
-        var otuLabelTen=otuLabelTen[testNum].slice(0,10); 
-        var trace={
-            x: otuValueTen,
-            y: otuIdTen.map(r=>`UTO ${r}`),
-            text: otuLabelTen,
-            type:'bar',
-            orientation:'h'
-        }
-        Plotly.newPlot('bar',[trace]);
-        // make bubble chart
-        var otuValue=samples.map(row=>row.sample_values);
-        var otuValue=otuValue[testNum];
-        var otuId=samples.map(row=>row.otu_ids);
-        var otuId=otuId[testNum];
-        var otuLabel=samples.map(row=>row.otu_labels); 
-        var otuLabel=otuLabel[testNum];
-        var minIds=d3.min(otuId);
-        var maxIds=d3.max(otuId);
-        var mapNr = d3.scaleLinear().domain([minIds, maxIds]).range([0, 1]);
-        var bubbleColors = otuId.map( val => d3.interpolateRgbBasis(["royalblue", "greenyellow", "goldenrod"])(mapNr(val)));
-        var trace1={
-            x: otuId,
-            y: otuValue,
-            text: otuLabel,
-            mode: 'markers',
-            marker: {
-                color: bubbleColors,
-                size: otuValue.map(x=>x*10),
-                sizemode: 'area'
-            }
-        };
-        var data1=[trace1];
-        var bubbleLayout={
-            xaxis:{
-                autochange: true,
-                height: 600,
-                width: 1000,
-                title: {
-                    text: 'OTU ID'
-                }
-            },
-        };
-        Plotly.newPlot('bubble',data1,bubbleLayout);   
-        // make gauge chart 
-        var meta=data.metadata;
-        var data2 = [
-            {
-                domain: { x: [0, 1], y: [0, 1] },
-                value: meta[testNum].wfreq,
-                title: { text: "Washing frequency" },
-                type: "indicator",
-                mode: "gauge+number",
-                gauge: { axis: { range: [null, 9] },
-                bar:{color: 'orange'},
-                   steps: [
-                    { range: [0, 2], color: "rgba(14, 127, 0, .5)" },
-                    { range: [2, 3], color: "rgba(110, 154, 22, .5)" },
-                    { range: [3, 4], color: "rgba(170, 202, 42, .5)" },
-                    { range: [4, 5], color: "rgba(202, 209, 95, .5)" },
-                    { range: [5, 6], color: "rgba(210, 206, 145, .5)" },
-                    { range: [6, 8], color: "rgba(232, 226, 202, .5)" },
-                    { range: [8, 9], color: "rgba(255, 255, 255, 0)" }
-                  ]}
-            }
-        ];
-        
-        var gaugeLayout = { width: 600, height: 500};
-        Plotly.newPlot('gauge', data2, gaugeLayout);
-        // display meta info
-        var metadata=d3.select('#sample-metadata');
-        metadata.html('');
-        Object.entries(meta[testNum]).forEach(([k,v])=>{
-            metadata.append('p').text(`${k.toUpperCase()}:\n${v}`);
-        })
-    })
-}
 
-// Submit Button handler
+function makePlot(nameID){
+    d3.json("data/samples.json").then((data) =>{
+        var samples = data.samples;
+        var samplesID = samples.map(row => row.id).indexOf(nameID);
+
+        // Make the bar plot
+        var sampleValues = samples.map(row => row.sample_values);
+        var sampleValues = sampleValues[samplesID].slice(0,10).reverse();
+        var otuIds = samples.map(row => row.otu_ids);
+        var otuIds = otuIds[samplesID].slice(0,10);
+        var otuLabels = samples.map(row => row.otu_labels); 
+        var otuLabels = otuLabels[samplesID].slice(0,10); 
+        var trace = {
+            x: sampleValues,
+            y: otuIds.map(r => `UTO ${r}`),
+            text: otuLabels,
+            type:"bar",
+            orientation:"h"
+        }
+        Plotly.newPlot("bar", [trace]);
+
+         // Make the bubble chart
+         var sampleValue = samples.map(row => row.sample_values);
+         var sampleValue = sampleValue[samplesID];
+         var otuId = samples.map(row => row.otu_ids);
+         var otuId = otuId[samplesID];
+         var otuLabel = samples.map(row => row.otu_labels); 
+         var otuLabel = otuLabel[samplesID];
+         var minId = d3.min(otuId);
+         var maxId = d3.max(otuId);
+         var mapNr = d3.scaleLinear().domain([minId, maxId]).range([0, 1]);
+         var colors = otuId.map(val => d3.interpolateRgbBasis(["royalblue", "greenyellow", "goldenrod"])(mapNr(val)));
+         var trace1 = {
+             x: otuId,
+             y: sampleValue,
+             text: otuLabel,
+             mode: "markers",
+             marker: {
+                 color: colors,
+                 size: sampleValue.map(x => x*10),
+                 sizemode: "area"
+             }
+         };
+         var bubbleLayout = {
+             xaxis:{
+                 autochange: true,
+                 height: 600,
+                 width: 1000,
+                 title: {
+                     text: "OTU ID"
+                 }
+             },
+         };
+         Plotly.newPlot("bubble", [trace1], bubbleLayout);
+        
+         // Make the gauge chart 
+         var meta = data.metadata;
+         var data1 = [
+             {
+                 domain: { x: [0, 1], y: [0, 1] },
+                 value: meta[samplesID].wfreq,
+                 title: { text: "Belly Button Washing Frequency" },
+                 type: "indicator",
+                 mode: "gauge+number",
+                 gauge: { axis: { range: [null, 9] },
+                 bar:{color: "orange"},
+                    steps: [
+                     { range: [0, 1], color: "rgba(248, 243, 236, 1)"},
+                     { range: [1, 2], color: "rgba(244, 241, 228, 1)"},
+                     { range: [2, 3], color: "rgba(233, 230, 201, 1)"},
+                     { range: [3, 4], color: "rgba(229, 232, 176, 1)"},
+                     { range: [4, 5], color: "rgba(213, 229, 153, 1)"},
+                     { range: [5, 6], color: "rgba(183, 205, 143, 1)"},
+                     { range: [6, 7], color: "rgba(138, 192, 134, 1)"},
+                     { range: [7, 8], color: "rgba(136, 188, 141, 1)"},
+                     { range: [8, 9], color: "rgba(132, 181, 136, 1)"}
+                   ]}
+             }
+         ];
+         
+         var gaugeLayout = { width: 600, height: 500};
+         Plotly.newPlot("gauge", data1, gaugeLayout);
+
+         // Make the meta info
+         var metadata = d3.select("#sample-metadata");
+         metadata.html('');
+         Object.entries(meta[samplesID]).forEach(([k,v]) => {
+             metadata.append('p').text(`${k.toUpperCase()}:\n${v}`);
+         })
+     })
+ }
+ 
+ // Make new plots if ID changed
 function optionChanged(newId) {
-    // Select the input value from the form
     makePlot(newId);
 }
