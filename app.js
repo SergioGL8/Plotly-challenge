21cf0aa7c2d263dfa4f7b84b115bd44931dee966
SergioGL <67619423+SergioGL8@users.noreply.github.com>
2020-11-07 08:37:22 -0600

    Plotly

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 d3.json("data/samples.json").then((data) => {
     var names = data.names;
     console.log(data.metadata);
-
-    var select = d3.selectAll("#selDataset");
+    var option = d3.selectAll("#selDataset");
     Object.entries(names).forEach(([i,v]) => {
-        select.append("option").text(v);
+        option.append("option").text(v);
     })
 })
 
